340dfb3f78396066e5eb8df8c8c6358ef293d396
github username <github email address>
2019-07-19 20:19:56 -0400

    add login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,25 @@ class SessionsController < ApplicationController
     end
 
     def new
-
     end
 
     def create
+        @user = User.find_by(email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else
+            flash[:message] = "Incorrect email or password"
+            redirect_to login_path
+        end
     end
-    
+
+    def destroy
+        session.clear
+        redirect_to root_path
+    end
+
+
+
 
 end
