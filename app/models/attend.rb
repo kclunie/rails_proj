75649015eccb2e0d0910533a7bcd7c5cceba0e1d
github username <github email address>
2019-07-26 11:14:18 -0400

    add user and attends validations

--- app/models/attend.rb
+++ app/models/attend.rb
@@ -2,7 +2,8 @@ class Attend < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  accepts_nested_attributes_for :user
+  #accepts_nested_attributes_for :user
+
   validates :guest, presence: true
   #scope :by_event, => (event_id) {where("event_id = ?", event_id)}
 
