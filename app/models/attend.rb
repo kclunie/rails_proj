29855e00136fc058ba2557c2bce4246aa4de5765
github username <github email address>
2019-07-26 09:57:57 -0400

    add event validations

--- app/models/attend.rb
+++ app/models/attend.rb
@@ -3,7 +3,7 @@ class Attend < ApplicationRecord
   belongs_to :event
 
   accepts_nested_attributes_for :user
-
+  validates :guest, presence: true
   #scope :by_event, => (event_id) {where("event_id = ?", event_id)}
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
     has_many :attends
     has_many :users, through: :attends
     validates :name, :date, :location, :details, presence: true
-
+    #validates :date, numericality: {message: "%(value) must be included"}
 
 
 end
