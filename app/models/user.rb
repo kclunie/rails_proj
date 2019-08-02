a8286075b46f8bc8a7906dba6ee31019ca197f2f
github username <github email address>
2019-08-01 21:58:26 -0400

    modified user model password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
     has_many :attends
     has_many :events, through: :attends
     has_secure_password
-    validates :name, :email, presence: true
+    validates :name, presence: true
     validates :email, uniqueness: true
 end
