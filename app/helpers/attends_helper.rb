e66e080224c8e7bd16a31df931ad505aec5dbf80
github username <github email address>
2019-08-07 14:29:11 -0400

    add logic to attends view

--- app/helpers/attends_helper.rb
+++ app/helpers/attends_helper.rb
@@ -1,2 +1,11 @@
 module AttendsHelper
+
+    def display_header
+        if params[:event_id]
+            content_tag(:h1, "RSVPs:")
+        else
+            content_tag(:h1, "My RSVPs")
+        end
+    end
+
 end
